00692a4fb708ddefa579194de932b5a95696e1e8
Andreas Lidholm <lidholm.andreas@gmail.com>
2024-12-30 07:59:43 +0100

    Add an uppercase function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,15 @@
 
 const {logger} = require("firebase-functions");
 const {onObjectFinalized} = require("firebase-functions/v2/storage");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const {getFirestore} = require("firebase-admin/firestore");
 
 exports.logstore = onObjectFinalized("my-bucket", (cloudEvent) => {
     logger.log(cloudEvent);
 });
+exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
+    const original = event.data.data().original;
+    logger.log("Uppercasing", event.params.documentId, original);
+    const uppercase = original.toUpperCase();
+    return event.data.ref.set({uppercase}, {merge: true});
+  });
