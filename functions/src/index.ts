79da0bf858b08be0e0f59e6d2e1614feb3f58be7
Andreas Lidholm <lidholm.andreas@gmail.com>
2024-12-30 07:33:02 +0100

    Delete all functions code

--- functions/src/index.ts
+++
@@ -1,3 +0,0 @@
-import {logstore} from "./logstore"; // Adjust path if necessary
-
-export {logstore};
--- functions/src/logstore.ts
+++
@@ -1,7 +0,0 @@
-import {onObjectFinalized} from "firebase-functions/v2/storage";
-import {logger} from "firebase-functions";
-
-export const logstore = onObjectFinalized("my-bucket", (cloudEvent) => {
-  logger.log(cloudEvent);
-});
-
