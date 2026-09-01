eb8096563cf809c55be430c67e6d4ad1de493aa6
Andreas Lidholm <lidholm.andreas@gmail.com>
2026-08-31 21:09:31 -0700

    melos bootstrap

--- widgetbook/macos/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
+++ widgetbook/macos/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -9,46 +9,46 @@ import PackageDescription
 let package = Package(
     name: "FlutterGeneratedPluginSwiftPackage",
     platforms: [
-        .macOS("10.15")
+        .macOS("12.0")
     ],
     products: [
         .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
     ],
     dependencies: [
-        .package(name: "url_launcher_macos", path: "../.packages/url_launcher_macos-3.2.5"),
-        .package(name: "shared_preferences_foundation", path: "../.packages/shared_preferences_foundation-2.5.6"),
-        .package(name: "package_info_plus", path: "../.packages/package_info_plus-8.3.1"),
-        .package(name: "firebase_core", path: "../.packages/firebase_core-4.13.0"),
-        .package(name: "firebase_auth", path: "../.packages/firebase_auth-6.5.7"),
-        .package(name: "firebase_analytics", path: "../.packages/firebase_analytics-12.4.6"),
-        .package(name: "device_info_plus", path: "../.packages/device_info_plus-11.5.0"),
         .package(name: "cloud_firestore", path: "../.packages/cloud_firestore-6.8.0"),
-        .package(name: "sqflite_darwin", path: "../.packages/sqflite_darwin-2.4.3"),
-        .package(name: "sign_in_with_apple", path: "../.packages/sign_in_with_apple-8.1.0"),
-        .package(name: "google_sign_in_ios", path: "../.packages/google_sign_in_ios-6.3.0"),
-        .package(name: "firebase_storage", path: "../.packages/firebase_storage-13.4.6"),
-        .package(name: "firebase_remote_config", path: "../.packages/firebase_remote_config-6.5.6"),
+        .package(name: "device_info_plus", path: "../.packages/device_info_plus-11.5.0"),
+        .package(name: "firebase_analytics", path: "../.packages/firebase_analytics-12.4.6"),
+        .package(name: "firebase_auth", path: "../.packages/firebase_auth-6.5.7"),
+        .package(name: "firebase_core", path: "../.packages/firebase_core-4.13.0"),
         .package(name: "firebase_crashlytics", path: "../.packages/firebase_crashlytics-5.2.7"),
+        .package(name: "firebase_remote_config", path: "../.packages/firebase_remote_config-6.5.6"),
+        .package(name: "firebase_storage", path: "../.packages/firebase_storage-13.4.6"),
+        .package(name: "google_sign_in_ios", path: "../.packages/google_sign_in_ios-6.3.0"),
+        .package(name: "package_info_plus", path: "../.packages/package_info_plus-8.3.1"),
+        .package(name: "shared_preferences_foundation", path: "../.packages/shared_preferences_foundation-2.5.6"),
+        .package(name: "sign_in_with_apple", path: "../.packages/sign_in_with_apple-8.1.0"),
+        .package(name: "sqflite_darwin", path: "../.packages/sqflite_darwin-2.4.3"),
+        .package(name: "url_launcher_macos", path: "../.packages/url_launcher_macos-3.2.5"),
         .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
     ],
     targets: [
         .target(
             name: "FlutterGeneratedPluginSwiftPackage",
             dependencies: [
-                .product(name: "url-launcher-macos", package: "url_launcher_macos"),
-                .product(name: "shared-preferences-foundation", package: "shared_preferences_foundation"),
-                .product(name: "package-info-plus", package: "package_info_plus"),
-                .product(name: "firebase-core", package: "firebase_core"),
-                .product(name: "firebase-auth", package: "firebase_auth"),
-                .product(name: "firebase-analytics", package: "firebase_analytics"),
-                .product(name: "device-info-plus", package: "device_info_plus"),
                 .product(name: "cloud-firestore", package: "cloud_firestore"),
-                .product(name: "sqflite-darwin", package: "sqflite_darwin"),
-                .product(name: "sign-in-with-apple", package: "sign_in_with_apple"),
-                .product(name: "google-sign-in-ios", package: "google_sign_in_ios"),
-                .product(name: "firebase-storage", package: "firebase_storage"),
-                .product(name: "firebase-remote-config", package: "firebase_remote_config"),
+                .product(name: "device-info-plus", package: "device_info_plus"),
+                .product(name: "firebase-analytics", package: "firebase_analytics"),
+                .product(name: "firebase-auth", package: "firebase_auth"),
+                .product(name: "firebase-core", package: "firebase_core"),
                 .product(name: "firebase-crashlytics", package: "firebase_crashlytics"),
+                .product(name: "firebase-remote-config", package: "firebase_remote_config"),
+                .product(name: "firebase-storage", package: "firebase_storage"),
+                .product(name: "google-sign-in-ios", package: "google_sign_in_ios"),
+                .product(name: "package-info-plus", package: "package_info_plus"),
+                .product(name: "shared-preferences-foundation", package: "shared_preferences_foundation"),
+                .product(name: "sign-in-with-apple", package: "sign_in_with_apple"),
+                .product(name: "sqflite-darwin", package: "sqflite_darwin"),
+                .product(name: "url-launcher-macos", package: "url_launcher_macos"),
                 .product(name: "FlutterFramework", package: "FlutterFramework")
             ]
         )
